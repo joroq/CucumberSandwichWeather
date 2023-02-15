e899fefabc0c7e23b887d98460203eaab66ddec6
Joanne Greenhalgh <joanne.greenhalgh@roq.co.uk>
2023-02-15 10:15:09 +0000

    Added a more complex test

--- list.test.js
+++ list.test.js
@@ -23,6 +23,18 @@ test("Simple CSS selector", async () => {
 test("Simple click", async () => {
     const element = await browser.getElement('action-1');
     await element.click();
-    const text = await (await browser.getElement('item-1')).getText();
+    const itemText = await browser.getElement('item-1');
+    const text = await itemText.getText();
     expect(text).toBe('A dozen eggs');
 });
+
+test("Add input and click", async () => {
+    const addedItem = 'My added item';
+    const input = await browser.getElement('new-item');
+    const submit = await browser.getElement('create-item');
+    await input.sendKeys(addedItem);
+    await submit.click();
+    const topItem = await browser.getElement('item-1');
+    const text = await topItem.getText();
+    expect(text).toBe(addedItem);
+})
